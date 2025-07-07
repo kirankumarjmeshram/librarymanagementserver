32222904add940f7aad83ee970ebcbf43feea153
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2025-07-07 12:06:48 +0530

    .

--- backend/routes/bookRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getBooks } = require("../controllers/bookController");
-
-router.get("/", getBooks);
-module.exports = router;
--- frontend/src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const api = axios.create({
-  baseURL: "http://localhost:5005/api",
-  withCredentials: true
-});
-export default api;
