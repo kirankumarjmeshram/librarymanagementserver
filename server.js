e396a693f15f59a4430bb90badba4d59ff709651
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2025-07-13 21:58:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: 'https://booklibrarymanagementapp.netlify.app',
   credentials: true
 }));
 app.use(express.json());
